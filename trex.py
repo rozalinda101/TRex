d98879b1eba07e56451e42f0acc1dc00d8d171bb
rozalinda101 <101523189+rozalinda101@users.noreply.github.com>
2022-05-23 23:03:06 +0200

    Update trex.py

--- trex.py
+++ trex.py
@@ -5,6 +5,7 @@ pygame.init()
 #rozmiar wyswietlacza określam
 rozmiar=szer,wys=700,400
 wyswietlacz=pygame.display.set_mode(rozmiar)
+pygame.display.set_caption("T-rex")
 
 #określam oznaczenia kolorow w rgb
 szary1=150,150,150
@@ -13,36 +14,27 @@ bialy=255,255,255
 
 #okreslam rozmiar "dinozaura"
 wys_dino=40
-szer_dino=20
+szer_dino=30
+
 
 #okreslam klasę obiektu dinozaur
 class dino():
     def __init__(self):
         #okreslam pozycje dinozaura na ekranie
-        self.wys_na_wysw=200
-        self.poz_na_szer=200
+        self.wys_na_wysw=200  #######CO OZNACZAJĄ SKRÓTY WYS_NA_WYSW POZ_NA_SZERA DO CZEGO TO SIĘ OD NOSI, NIE MOGĘ TEGO ZROZUMIEĆ, HELP XD
+        self.poz_na_szera=200
         #przypisuje rozmiar dinozaura
         self.wys=wys_dino
         self.szer=szer_dino
 
-    #rysuję "dinozaura"
+    #rysuję dino
     def narysuj(self,wyswietlacz):
-        pygame.draw.rect(wyswietlacz,szary2,[self.poz_na_szer,self.wys_na_wysw,self.szer,self.wys])
+        pygame.draw.rect(wyswietlacz,szary2,[self.poz_na_szera,self.wys_na_wysw,self.szer,self.wys])
 
-    #wymazuję dinozaura przy skakaniu
-    def zniknij(self,wyswietlacz):
-        pygame.draw.rect(wyswietlacz,bialy,[self.poz_na_szer,self.wys_na_wysw,self.szer,self.wys])
 
     #skakanie - zmieniam pozycję, wymazuję tego co na ziemi, chwila skoku - 0,5 sekundy, zmieniam pozycję znowy na ziemię
     def skakanie(self,wyswietlacz):
-        self.zniknij(wyswietlacz)
-        self.wys_na_wysw-=70
-        self.narysuj(wyswietlacz)
-        pygame.display.update()
-        time.sleep(0.5)
-        self.zniknij(wyswietlacz)
-        self.wys_na_wysw+=70
-        self.narysuj(wyswietlacz)
+        self.wys_na_wysw -= 70
         pygame.display.update()
 
 
@@ -50,27 +42,24 @@ class dino():
 class kaktus():
     def __init__(self):
         #okreslam pozycje kaktusa na ekranie
-        self.wys_na_wysw=180
+        self.wys_na_wysw=190
         self.poz_na_szer=700
         #przypisuje rozmiar kaktusa
-        self.wys=60
+        self.wys=50
         self.szer=20
+
 
     #rysuję "kaktusa"
     def narysuj(self,wyswietlacz):
         pygame.draw.rect(wyswietlacz,szary2,[self.poz_na_szer,self.wys_na_wysw,self.szer,self.wys])
 
-    #wymazuję kaktusa przy poruszaniu
-    def zniknij(self,wyswietlacz):
-        pygame.draw.rect(wyswietlacz,bialy,[self.poz_na_szer,self.wys_na_wysw,self.szer,self.wys])
 
     #poruszanie - zmieniam pozycję, wymazuję, zmieniam pozycję znowu
     def poruszanie(self,wyswietlacz):
-        self.zniknij(wyswietlacz)
-        self.poz_na_szer-=3
+        self.poz_na_szer-=0.1
         self.narysuj(wyswietlacz)
         pygame.display.update()
-        time.sleep(0.1)
+
 
 
 dinozaur=dino()
@@ -99,8 +88,7 @@ while True:
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_SPACE:
                 dinozaur.skakanie(wyswietlacz)
-                #żeby nie dało się cały czas skakać
-                time.sleep(0.5)
+
 
     pygame.display.update()
 
