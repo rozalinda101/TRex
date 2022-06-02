db1428b59fd4e51a03b42cac8e9219ead7c73541
paulazgr <104891682+paulazgr@users.noreply.github.com>
2022-06-02 21:41:21 +0200

    Update t-rex.py
    trzy kaktusy na ekranie i poprawione koordynaty x żeby się gra zatrzymywała od razu po uderzeniu w kaktusa

--- t-rex.py
+++ t-rex.py
@@ -3,7 +3,6 @@ import time
 import random
 
 # Stworzenie okna
-
 okno = turtle.Screen()
 okno.title("T-Rex")
 okno.bgcolor("white")
@@ -13,8 +12,8 @@ okno.tracer(0)
 POZIOM_PODŁOGI = -60
 GRAWITACJA = -0.004
 punkty=0
-# Stworzenie podłogi
 
+# Stworzenie podłogi
 podloga = turtle.Turtle()
 podloga.speed(0)
 podloga.shape("square")
@@ -27,15 +26,11 @@ punkty_aktualne=0
 
 czas_pocz=time.time()
 
-
-
-
-
 # Kaktus 1
 kaktus_1 = turtle.Turtle()
 kaktus_1.speed(0)
 kaktus_1.shape("square")
-kaktus_1.color("black")
+kaktus_1.color("green")
 kaktus_1.shapesize(stretch_wid=2, stretch_len=1)
 kaktus_1.penup()
 pozycja = kaktus_1.goto(random.randint(300,400), -60)
@@ -45,11 +40,21 @@ kaktus_1.dx = -0.6
 kaktus_2 = turtle.Turtle()
 kaktus_2.speed(0)
 kaktus_2.shape("square")
-kaktus_2.color("grey")
+kaktus_2.color("green")
 kaktus_2.shapesize(stretch_wid=2, stretch_len=1)
 kaktus_2.penup()
-pozycja = kaktus_2.goto(random.randint(-50,100), -60)
+pozycja = kaktus_2.goto(random.randint(600,700), -60)
 kaktus_2.dx = -0.6
+
+# Kaktus 3
+kaktus_3 = turtle.Turtle()
+kaktus_3.speed(0)
+kaktus_3.shape("square")
+kaktus_3.color("green")
+kaktus_3.shapesize(stretch_wid=2, stretch_len=1)
+kaktus_3.penup()
+pozycja = kaktus_3.goto(random.randint(800,900), -60)
+kaktus_3.dx = -0.6
 
 # Dino
 dino = turtle.Turtle()
@@ -62,9 +67,6 @@ dino.dy = 0
 dino.stan = "biegnie"
 dino.goto(-250, -60)
 
-
-
-
 def skakanie():
     if dino.stan == "biegnie":
         dino.dy = 0.7
@@ -76,11 +78,8 @@ okno.onkeypress(skakanie, "space")
 def restart():
     okno_restart=turtle.Turtle()
     okno_restart.speed(0)
-    okno_restart.shape("square")
-    okno_restart.color("blue")
-    okno_restart.shapesize(stretch_wid=5,stretch_len=10)
-    okno_restart.write(f"KONIEC GRY! Zdobyłeś {punkty_aktualne} punktów", align = "center", font=("Courier",10,"normal"))
-
+    okno_restart.color("red")
+    okno_restart.write(f"KONIEC GRY! Zdobyłeś {punkty_aktualne} punktów", align = "center", font=("Courier",15,"normal"))
 
 # Główna pętla gry
 while True:
@@ -118,37 +117,51 @@ while True:
     x += kaktus_1.dx
     kaktus_1.setx(x)
 
-    
     if kaktus_1.xcor() < -400:
         x = random.randint(400, 600)
         kaktus_1.setx(x)
         kaktus_1.dx *= 1.05
         print(kaktus_1.dx)
-        
+
     x = kaktus_2.xcor()
     x += kaktus_2.dx
     kaktus_2.setx(x)
-
 
     if kaktus_2.xcor() < -400:
         x = random.randint(400, 600)
         kaktus_2.setx(x)
         kaktus_2.dx *= 1.05
         print(kaktus_2.dx)
-        
+
+    x = kaktus_3.xcor()
+    x += kaktus_3.dx
+    kaktus_3.setx(x)
+
+    if kaktus_3.xcor() < -400:
+        x = random.randint(400, 600)
+        kaktus_3.setx(x)
+        kaktus_3.dx *= 1.05
+        print(kaktus_2.dx)
+
     #kolizja z jednym kaktusem
-    if dino.xcor()>kaktus_1.xcor()-10 and dino.xcor()<kaktus_1.xcor()+10 and dino.ycor()<=kaktus_1.ycor():
-        dino.setx(pozycja)
-        dino.sety(pozycja)
-        restart()
-        turtle.done()
-        
-    #kolizij z drugim kaktusem
-    if dino.xcor()>kaktus_2.xcor()-10 and dino.xcor()<kaktus_2.xcor()+10 and dino.ycor()<=kaktus_2.ycor():
+    if dino.xcor()>kaktus_1.xcor()-25 and dino.xcor()<kaktus_1.xcor()+40 and dino.ycor()<=kaktus_1.ycor():
         dino.setx(pozycja)
         dino.sety(pozycja)
         restart()
         turtle.done()
 
+    #kolizja z drugim kaktusem
+    if dino.xcor()>kaktus_2.xcor()-25 and dino.xcor()<kaktus_2.xcor()+40 and dino.ycor()<=kaktus_2.ycor():
+        dino.setx(pozycja)
+        dino.sety(pozycja)
+        restart()
+        turtle.done()
+
+    #kolizja z trzecim kaktusem
+    if dino.xcor()>kaktus_3.xcor()-25 and dino.xcor()<kaktus_3.xcor()+40 and dino.ycor()<=kaktus_3.ycor():
+        dino.setx(pozycja)
+        dino.sety(pozycja)
+        restart()
+        turtle.done()
 
     okno.update()
